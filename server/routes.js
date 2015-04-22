13a5c075a8e7fad3d46b2a6ff7b44ae924145710
pandeysoni <sonipandey.71@gmail.com>
2015-04-22 14:55:01 +0530

    changes in routes

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,16 @@
 // Load modules
 
-var User      = require('./controller/user'),
+var Rule      = require('./controller/rule'),
   Static    = require('./static');
 
 // API Server Endpoints
 exports.endpoints = [
 
   { method: 'GET',  path: '/{somethingss*}', config: Static.get },
-  { method: 'POST', path: '/user', config: User.create},
-  { method: 'GET', path: '/user', config: User.getAll},
-  { method: 'GET', path: '/user/{userId}', config: User.getOne},
-  { method: 'PUT', path: '/user/{userId}', config: User.update},
-  { method: 'DELETE', path: '/user/{userId}', config: User.remove},
-  { method: 'DELETE', path: '/user', config: User.removeAll}
+  { method: 'POST', path: '/rule', config: Rule.create},
+  { method: 'GET', path: '/rule', config: Rule.getAll},
+  { method: 'GET', path: '/rule/{id}', config: Rule.getOne},
+  { method: 'PUT', path: '/rule/{id}', config: Rule.update},
+  { method: 'DELETE', path: '/rule/{id}', config: Rule.remove},
+  { method: 'DELETE', path: '/rule', config: Rule.removeAll}
 ];
