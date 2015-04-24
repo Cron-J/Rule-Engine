04efe070ff2fe3c0e4e76a1c6f28c3ccd88cb491
pandeysoni <sonipandey.71@gmail.com>
2015-04-24 18:03:19 +0530

    some changes

--- client/src/app/rule/ruleCtrl.js
+++ client/src/app/rule/ruleCtrl.js
@@ -1,13 +1,32 @@
 app.controller('ruleCtrl', [ '$scope', '$http','$location', 'growl',
 	function($scope, $http, $location,  growl){
+		$scope.changeView.ruleEditPage = true;
+		$scope.changeView.expressionEditPage = false;
+		$scope.changeView.actionEditPage = false;
 
 		$scope.add_new_rule = function(){
 			$scope.changeView.ruleHomeShow = true;
 		}
 
 		$scope.rule_editor_page = function(){
-			// $scope.changeView.ruleHomeShow = true;
-			$scope.changeView.ruleEditorShow = true;
+			$scope.changeView.ruleEditPage = true;
+			$scope.changeView.expressionEditPage = false;
+			$scope.changeView.actionEditPage = false;
+			console.log("Hi");
+		}
+
+		$scope.expression_editor_page = function(){
+			$scope.changeView.ruleEditPage = false;
+			$scope.changeView.expressionEditPage = true;
+			$scope.changeView.actionEditPage = false;
+			console.log("null");
+		}
+
+		$scope.action_editor_page = function(){
+			$scope.changeView.ruleEditPage = false;
+			$scope.changeView.expressionEditPage = false;
+			$scope.changeView.actionEditPage = true;
+			console.log("In action");
 		}
 	}]
 )
